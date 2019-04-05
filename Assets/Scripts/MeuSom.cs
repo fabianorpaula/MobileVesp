af57d4083300399f52c2dbaa410944608e6d7ad3
Fabiano De Paula <fabiano.rpaula@gmail.com>
2019-04-05 19:17:21 -0300

    Mixer

---
+++ Assets/Scripts/MeuSom.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeuSom : MonoBehaviour
+{
+    //Dano
+    public AudioClip dano;
+    //Ponto
+    public AudioClip ponto;
+
+    //moeda
+    public AudioClip moeda;
+
+
+    //EMISOR DE SOM
+    private AudioSource fontedesom;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        fontedesom = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //TOCAR DANO
+    public void SomDano(){
+        fontedesom.clip = dano;
+        fontedesom.Play();
+    }
+
+    //TOCAR PONTO
+    public void SomPonto(){
+        fontedesom.clip = ponto;
+        fontedesom.Play();
+    }
+
+    //TOCAR MOEDA
+    public void SomMoeda(){
+        fontedesom.clip = moeda;
+        fontedesom.Play();
+    }
+
+
+}
